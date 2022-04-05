0e978aaf950a208cfeb3bd8130dff92aa80bd273
Ranjit Bhakta <ranjitbhakta>
2022-04-05 09:31:43 +0530

    Modifying URL

--- client/src/app/services/video.service.ts
+++ client/src/app/services/video.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { api } from "../models/API";
 
 @Injectable({
   providedIn: "root",
 })
 export class VideoService {
-  API_URI = api;
   constructor(private http: HttpClient) {}
 
   getVideos() {
-    return this.http.get(`${this.API_URI}/videos`);
+    return this.http.get(`/api/videos`);
   }
 
   processVideo(data: any) {
-    return this.http.post(`${this.API_URI}/video/process`, data);
+    return this.http.post(`/api/video/process`, data);
   }
 
   getOutputVideo() {
-    return this.http.get(`${this.API_URI}/video`);
+    return this.http.get(`/api/video`);
   }
 
   checkOutputVideo() {
-    return this.http.get(`${this.API_URI}/video/check`);
+    return this.http.get(`/api/video/check`);
   }
 
   uploadVideo(data: any) {
-    return this.http.post(`${this.API_URI}/video`, data);
+    return this.http.post(`/api/video`, data);
   }
 }
